1ad9e9629f7aace434344bd1d4dbcdf07b2d581e
JuanHernandezVe <jj.hernandezv1@uniandes.edu.co>
2025-02-06 21:11:30 -0500

    Funciones addfirst y size

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -29,5 +29,39 @@ def is_present(my_list, element, cmp_function):
     if not is_in_array:
         count = -1
     return count
+
+def new_list ():
+    new_list = {
+        "first" : None,
+        "last" : None,
+        "size" : 0,
+    }
+    return new_list
         
+        
+def new_single_node(element):
+    
+    return {"info": element, "next": None}
+
+
+def add_first(my_list, element):
+    nodo = new_single_node(element)
+    
+    nodo["next"] = my_list["first"]
+    my_list["first"] = nodo
+    
+    if my_list["size"] == 0:
+        my_list["last"] = my_list["first"]
+        
+    my_list["size"] += 1
+    
+    return my_list
+
+def size(my_list):
+    size = my_list["size"]
+    return size
+    
+
+    
+    
     
